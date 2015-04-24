f8f29369c1bb1e912cad5bb8dc3501f7671626bc
Doug Duril <doug.duril@gmail.com>
2015-04-23 18:54:59 -0700

    Second commit
    Adding php template and first sample file.

---
+++ lib/template.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Doug
+ * Date: 4/23/2015
+ * Time: 6:45 PM
+ */
+function loadTop(){
+    ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>HTML5 Samples</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="../css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/bootstrap-theme.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="../css/starter-template.css" rel="stylesheet">
+        <link href="../css/jumbotron.css" rel="stylesheet">
+        <link href="../css/theme.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">HTML5</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="../index.html">Home</a></li>
+                    <!--<li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>-->
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <style>
+        .jumbotron {
+            margin-bottom: .5em;
+            padding: 1.8em 1.8em;
+            background-color: #369;
+            color: #fff;
+        }
+
+        .jumbotron p {
+            font-size: 1.2em;
+        }
+    </style>
+    <div class="jumbotron">
+        <div class="container">
+            <h1>HTML5 Samples</h1>
+            <p></p>
+        </div>
+    </div>
+<?php
+}
+?>
+
+<?php
+function loadBottom(){
+    ?>
+<hr>
+
+    <footer>
+        <p>&copy; </p>
+    </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../js/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+
+  </body>
+</html>
+<?php
+}
+?>
---
+++ pages/survey-form.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Doug
+ * Date: 4/23/2015
+ * Time: 6:44 PM
+ */
+
+include ("../lib/template.php");
+
+loadTop();
+?>
+
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-12">
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris
+                    condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod.</p>
+
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+
+
+<?php
+
+loadBottom();
+?>
